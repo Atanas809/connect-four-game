804be5c0a5929c9efc92d3a19a0886603990dfd3
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-12 10:46:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 def is_valid_indices(value, rows, cols):
 
     valid = []
+
+    for index in value:
+        row, col = index[0], index[1]
+        if 0 <= row < rows and 0 <= col < cols:
+            valid.append([row, col])
+
+    return valid
