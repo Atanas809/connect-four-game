42839738e8270395a7a760ed205ae526b1709d9d
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-12 10:49:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,3 +50,12 @@ def is_winning(board, current_player, make_move, free_rows,rows, cols):
             [row + 3, col + 3],
 
         ],
+        "secondary_diagonal": [
+            [row, col],
+            [row + 1, col - 1],
+            [row + 2, col - 2],
+            [row + 3, col - 3],
+
+        ]
+
+    }
